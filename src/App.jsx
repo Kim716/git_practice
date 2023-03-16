158b8b8a21b0969960f9741dec6b892d5cd14f52
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-16 16:38:53 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Button from "./components/Button";
 
 function App() {
   const handleLoginClick = () => {
-    console.log("login ！！！！");
+    console.log("login");
   };
 
   return <Button buttonName={"click"} onClick={handleLoginClick} />;
